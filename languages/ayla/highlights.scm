038a766926ae2a063c1647795aaecc8b8dd38944
Ziad Skafi <ziad.skafi12@gmail.com>
2026-02-13 23:53:03 +0400

    updated highlights

--- languages/ayla/highlights.scm
+++ languages/ayla/highlights.scm
@@ -1,32 +1,4 @@
-(keyword: $ => choice(
-  "egg",
-  "rock",
-  "fun",
-  "type",
-  "struct",
-  "enum",
-  "int",
-  "float",
-  "string",
-  "bool",
-  "thing",
-  "map",
-  "ayla",
-  "elen",
-  "four",
-  "why",
-  "back",
-  "kitkat",
-  "next",
-  "decide",
-  "when",
-  "otherwise",
-  "spawn",
-  "with",
-  "it",
-  "in",
-  "range"
-)) @keyword
+(keyword) @keyword
 
 (boolean) @constant.builtin
 (nil) @constant.builtin

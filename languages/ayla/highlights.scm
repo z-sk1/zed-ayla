ee56018cc9ec6569bb81437ec795ac9593ed064b
Ziad Skafi <ziad.skafi12@gmail.com>
2026-02-13 23:12:46 +0400

    separated tree-sitter

---
+++ languages/ayla/highlights.scm
@@ -0,0 +1,30 @@
+; Keywords
+"struct" @keyword
+"fun" @keyword
+"yes" @constant.builtin
+"no" @constant.builtin
+"nil" @constant.builtin
+
+; Types
+(type_identifier) @type
+(struct_decl
+  name: (type_identifier) @type.definition)
+
+; Functions
+(function_decl
+  name: (identifier) @function.definition)
+
+(call_expression
+  function: (identifier) @function.call)
+
+; Variables
+(identifier) @variable
+(member_expression
+  property: (identifier) @variable.other.member)
+
+; Literals
+(string) @string
+(number) @number
+
+; Comments
+(comment) @comment
